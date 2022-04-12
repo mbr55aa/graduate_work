ac2c34e8ac54e079742b254b52587f6630ec4953
Alexey Nikitin <mbr55aa@gmail.com>
2022-04-12 21:44:39 +0300

    Use UTF8 for JSON

--- assistant/app.py
+++ assistant/app.py
@@ -15,6 +15,8 @@ logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
 
+app.config['JSON_AS_ASCII'] = False
+
 app.register_blueprint(blueprint_alice)
 app.register_blueprint(blueprint_bob)
 
