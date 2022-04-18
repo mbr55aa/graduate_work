98b75c2c349119bda12cdf464864b9b250ad2719
sergey <sergei.astapchik@yandex.ru>
2022-04-18 22:07:46 +0300

    change docstrings

--- assistant/connectors/search.py
+++ assistant/connectors/search.py
@@ -14,28 +14,44 @@ class SearchConnector:
 
     SEARCH_API_URL = None
 
-    def __init__(self):
-        pass
-
     def find_film_data(self, search_str: str) -> Optional[Film]:
         """
-        Find all info about the requested film
-        @param search_str: film name
-        @return: Film or None
+        Find all info about the requested film.
+
+        :param search_str: film name
+        :return: Film or None
         """
         film_uuid = self.find_film_uuid(search_str)
         film = self._get_film_by_uuid(film_uuid)
         return film
 
     def find_film_title(self, search_str: str) -> str:
+        """
+        Find title of the requested film.
+
+        :param search_str: film name
+        :return: Title or None
+        """
         film = self.find_film_data(search_str)
         return getattr(film, 'title', None)
 
     def find_film_rating(self, search_str: str) -> str:
+        """
+        Find rating of the requested film.
+
+        :param search_str: film name
+        :return: rating or None
+        """
         film = self.find_film_data(search_str)
         return getattr(film, 'imdb_rating', None)
 
     def find_film_genres(self, search_str: str) -> List[str]:
+        """
+        Find genres of the requested film.
+
+        :param search_str: film name
+        :return: Genres or None
+        """
         film = self.find_film_data(search_str)
         genres_names = []
         for genre in getattr(film, 'genre', []):
@@ -44,40 +60,50 @@ class SearchConnector:
 
     def find_film_directors(self, search_str: str) -> List[str]:
         """
-        Find directors(s) of the requested film
-        @param search_str: film name
-        @return: list of directors' names or None
+        Find directors(s) of the requested film.
+
+        :param search_str: film name
+        :return: list of directors' names or None
         """
         film = self.find_film_data(search_str)
         return getattr(film, 'directors_names', None)
 
     def find_film_actors(self, search_str: str) -> List[str]:
         """
-        Find actors of the requested film
-        @param search_str: film name
-        @return: list of actors' names or  None
+        Find actors of the requested film.
+
+        :param search_str: film name
+        :return: list of actors' names or  None
         """
         film = self.find_film_data(search_str)
         return getattr(film, 'actors_names', None)
 
-    def find_film_description(self, search_str: str) -> List[str]:
+    def find_film_description(self, search_str: str) -> str:
+        """
+        Find description of the requested film.
+
+        :param search_str: film name
+        :return: Description or None
+        """
         film = self.find_film_data(search_str)
         return getattr(film, 'description', None)
 
     def find_film_writers(self, search_str: str) -> List[str]:
         """
-        Find writers of the requested film
-        @param search_str: film name
-        @return: list of writer's names or None
+        Find writers of the requested film.
+
+        :param search_str: film name
+        :return: list of writer's names or None
         """
         film = self.find_film_data(search_str)
         return getattr(film, 'writers_names', None)
 
     def find_film_uuid(self, search_str: str) -> Optional[UUID]:
         """
-        Find UUID of the requested film
-        @param search_str: film name
-        @return: UUID of the film that best matches the request or None
+        Find UUID of the requested film.
+
+        :param search_str: film name
+        :return: UUID of the film that best matches the request or None
         """
         response = self._get_response(
             "film/search",
@@ -93,9 +119,10 @@ class SearchConnector:
 
     def _get_film_by_uuid(self, film_uuid: UUID) -> Optional[Film]:
         """
-        Get film data by UUID
-        @param film_uuid: Film UUID
-        @return: Film or None
+        Get film data by UUID.
+
+        :param film_uuid: Film UUID
+        :return: Film or None
         """
         response = self._get_response(f"film/{film_uuid}")
         if not response:
@@ -104,10 +131,11 @@ class SearchConnector:
 
     def _get_response(self, path: str, query: Optional[dict] = None) -> Optional[dict]:
         """
-        Make response to Async API
-        @param path: web path of response
-        @param query: query params of GET request
-        @return: data in JSON format
+        Make response to Async API.
+
+        :param path: web path of response
+        :param query: query params of GET request
+        :return: data in JSON format
         """
         response = requests.get(self._get_api_url() + path, params=query)
         if response.status_code != HTTPStatus.OK:
@@ -116,8 +144,9 @@ class SearchConnector:
 
     def _get_api_url(self) -> str:
         """
-        Return URL of Async API
-        @return: string with Async API URL
+        Get URL of Async API.
+
+        :return: string with Async API URL
         """
         if not self.SEARCH_API_URL:
             self.SEARCH_API_URL = config.SEARCH_API_URL
