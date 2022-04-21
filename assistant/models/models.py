fc7f014f146aa102e92b789916bbd2a5f49fc08e
pavlom <10door@gmail.com>
2022-04-21 17:39:08 +0300

    Add orjson to models

--- assistant/models/models.py
+++ assistant/models/models.py
@@ -1,29 +1,40 @@
 from typing import List, Optional
 from uuid import UUID
 
+import orjson
 from pydantic import BaseModel
 
 
-class PersonBase(BaseModel):
+def orjson_dumps(v, *, default):
+    return orjson.dumps(v, default=default).decode()
+
+
+class AbstractModel(BaseModel):
+    class Config:
+        json_loads = orjson.loads
+        json_dumps = orjson_dumps
+
+
+class PersonBase(AbstractModel):
     """Информация о персоне."""
     uuid: UUID
     full_name: str
 
 
-class GenreBase(BaseModel):
+class GenreBase(AbstractModel):
     """Информация о жанре."""
     uuid: UUID
     name: str
 
 
-class FilmBase(BaseModel):
+class FilmBase(AbstractModel):
     """Информация о фильме."""
     uuid: UUID
     title: str
     imdb_rating: float
 
 
-class Film(BaseModel):
+class Film(AbstractModel):
     """Подробная информация о фильме."""
     uuid: UUID
     title: str
@@ -37,13 +48,13 @@ class Film(BaseModel):
     directors_names: Optional[List[str]]
 
 
-class Films(BaseModel):
+class Films(AbstractModel):
     """Список фильмов."""
     genre: Optional[str]
     film_ids: Optional[FilmBase]
 
 
-class Genre(BaseModel):
+class Genre(AbstractModel):
     """Подробная информация о жанре."""
     uuid: UUID
     name: str
@@ -52,7 +63,7 @@ class Genre(BaseModel):
     film_detailed_ids: Optional[List[Film]]
 
 
-class Person(BaseModel):
+class Person(AbstractModel):
     """Подробная информация о персоне."""
     uuid: UUID
     full_name: str
